138d0f222301202d2dc6c9c6da79f0221a7da802
Joe <josephbeasleythomas@gmail.com>
2021-03-01 17:27:18 -0500

    looping api call

--- request_json/appjsoncall.js
+++ request_json/appjsoncall.js
@@ -2,8 +2,10 @@ var request = require('request');
 var url = process.argv.slice(2);
 
 console.log('url: ', url);
-request(url[0], function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
-    }
-})
+const interval = setInterval(function() {
+    request(url[0], function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Print the google web page.
+        }
+    })
+}, 10000);
